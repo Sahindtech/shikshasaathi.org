de565a86d3536318f7574c33fa8c484ae41f4ad2
Adit129 <shuklaaditi129@gmail.com>
2024-02-17 20:42:08 +0530

    initiated Project

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,75 @@
+import React, { useRef } from "react";
+import { Container } from "reactstrap";
+import "./header.css";
+
+const navLinks = [
+  {
+    display: "Home",
+    url: "#",
+  },
+  {
+    display: "About",
+    url: "#",
+  },
+
+  {
+    display: "Courses",
+    url: "#",
+  },
+  {
+    display: "Student Corner",
+    url: "#",
+  },
+  {
+    display: "Certificate View",
+    url: "#",
+  },
+  {
+    display: "Gallery",
+    url: "#",
+  },
+  {
+    display: "Contact Us",
+    url: "#",
+  },
+];
+
+const Header = () => {
+  const menuRef = useRef();
+
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+
+  return (
+    <header className="header">
+      <Container>
+        <div className="navigation d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <h2 className=" d-flex align-items-center gap-1">
+              <i class="ri-pantone-line"></i> Shikshasaathi
+            </h2>
+          </div>
+
+          <div className="nav d-flex align-items-center gap-5">
+            <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
+              <ul className="nav__list">
+                {navLinks.map((item, index) => (
+                  <li key={index} className="nav__item">
+                    <a href={item.url}>{item.display}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="mobile__menu">
+            <span>
+              <i class="ri-menu-line" onClick={menuToggle}></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "../components/Header/Header";
+import HeroSection from "../components/Hero-Section/HeroSection";
+const Home = () => {
+  return (
+    <Fragment>
+      <Header />
+      <HeroSection />
+    </Fragment>
+  );
+};
+
+export default Home;
