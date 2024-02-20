74aff0f24915af189b62e2a0e482c57f6a996bf8
Adit129 <shuklaaditi129@gmail.com>
2024-02-20 19:07:19 +0530

    footer updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import Header from "../components/Header/Header";
 import HeroSection from "../components/Hero-Section/HeroSection";
+import Footer from "../components/Footer/Footer";
 const Home = () => {
   return (
     <Fragment>
       <Header />
       <HeroSection />
+      <Footer />
     </Fragment>
   );
 };
