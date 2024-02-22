92a9d50b751fbf459753c98584fcc2968b2bf13c
GulafsaJahan <132530666+GulafsaJahan@users.noreply.github.com>
2024-02-22 18:02:23 +0530

    Add About-us and Courses

---
+++ frontend/src/components/Courses/courses.jsx
@@ -0,0 +1,173 @@
+import React from 'react'
+import { Card, Button,Col, Container, Row } from 'react-bootstrap';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import "./course.css";
+
+const Courses = () => {
+  return (
+    <>
+    <Header />
+   <Container fluid>
+     
+        <Row>
+            <Col className='col-12 text-center py-4'>
+                <h1>Our Courese</h1>
+            </Col>
+        </Row>
+        <Container>
+        <Row>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            <Col lg="3" md="6">
+            <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                     <Card.Title>MCA</Card.Title>
+             <Card.Text>
+                 Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+                <Button variant="primary">About more info.. </Button>
+            </Card.Body>
+            </Card>
+            </Col>
+            
+        </Row>
+    </Container>
+   </Container>
+   <Footer />
+   </>
+  );
+};
+
+export default Courses;
