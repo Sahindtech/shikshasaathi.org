63ebf6c5f3be774a0de2514ff002d95ddf5b35b1
GulafsaJahan <132530666+GulafsaJahan@users.noreply.github.com>
2024-02-24 13:38:37 +0530

    Update Student

---
+++ frontend/src/components/Student Corner/Student.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import "./studentCorner.css";
+import { Container, Row, Col } from 'reactstrap';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
+const StudentCorner = () => {
+    return(
+    <>
+    <Header />
+    <Container fluid>
+        <Row>
+            <Col className='col-12 text-center py-4'>
+                <h1 style={{color: 'white'}} className='headerImg'>Student's Corner</h1>
+            </Col>
+        </Row>
+        <form id="contact_form" name="contact_form" method="post">
+        <h1>Student's Corner</h1>
+        <div className="mb-3 row">
+          <div className="col-sm-6">
+            <label>User name</label>
+            <input
+              type="text"
+              required
+              maxLength="50"
+              className="form-control"
+              id="username"
+              name="username"
+              placeholder='Enter your username '
+            />
+          </div>
+          </div>
+          <div className="mb-3 row">
+          <div className="col-sm-6">
+            <label htmlFor="email_addr">Email address</label>
+            <input
+              type="email"
+              required
+              maxLength="50"
+              className="form-control"
+              id="email_addr"
+              name="email"
+              placeholder="name@example.com"
+            />
+          </div>
+          </div>
+          <button type="submit" className='btn btn-primary btngrp'>
+          Submit
+        </button>
+          </form>
+        </Container>
+    <Footer />
+    </>
+);
+};
+
+export default StudentCorner;
